32fc28ab8b33ed85ad18e2b1b6bb8713d82d7da7
rohanbabbar04 <rohanbabbar0408@yahoo.com>
2023-05-15 17:51:02 +0530

    Initial Setup commit

---
+++ pylops_mpi/DistributedArray.py
@@ -0,0 +1,85 @@
+import numpy as np
+from typing import Optional
+
+from mpi4py import MPI
+
+from pylops.utils import DTypeLike, NDArray
+
+
+class DistributedArray:
+
+    def __init__(self, data: NDArray,
+                 base_comm: Optional[MPI.Comm] = MPI.COMM_WORLD,
+                 dtype: Optional[DTypeLike] = 'float',
+                 broad_cast: Optional[bool] = False):
+        self.base_comm = base_comm
+        self.dtype = dtype
+        self.size = self.base_comm.Get_size()
+        self.rank = self.base_comm.Get_rank()
+        self.shape = data.shape
+        self.broad_cast = broad_cast
+        self._local_array = self.distributed(data)
+
+    def distributed(self, x: NDArray):
+        if np.isscalar(x):
+            self.size = 1
+            return x
+        if self.broad_cast:
+            self._broadcast(x)
+        else:
+            self._scatter(x)
+        return self._local_array
+
+    def _broadcast(self, x: NDArray = None):
+        if x is not None:
+            self._local_array = np.empty(x.shape, dtype=self.dtype)
+            if self.rank == 0:
+                self._local_array = x.astype(self.dtype)
+        self.base_comm.Bcast(self._local_array, root=0)
+
+    def _scatter(self, x: NDArray):
+        send_data = None
+        if self.rank == 0:
+            arrs = np.array_split(x, self.size)
+            raveled = [np.ravel(arr) for arr in arrs]
+            send_data = np.concatenate(raveled, dtype=self.dtype)
+        recv_shape = list(x.shape)
+        recv_shape_new = [((recv_shape[0] // self.size) + 1) if self.rank < (
+            recv_shape[0] % self.size)
+            else recv_shape[0] // self.size] + recv_shape[1:]
+        self._local_array = np.empty(recv_shape_new, dtype=self.dtype)
+        self.base_comm.Scatterv(send_data, self._local_array, root=0)
+
+    def get_local_arrays(self):
+        return self.base_comm.allgather(self._local_array)
+
+    def __add__(self, x):
+        return self.add(x)
+
+    def __mul__(self, x):
+        return self.multiply(x)
+
+    def asarray(self):
+        return np.concatenate(self.get_local_arrays()).reshape(self.shape)
+
+    def add(self, dist_array):
+        if self.shape != dist_array.shape:
+            raise ValueError("Shape Mismatch")
+        recv_buff = np.empty(shape=self.shape, dtype=self.dtype)
+        self.base_comm.Allgatherv(self._local_array + dist_array._local_array,
+                                  recv_buff)
+        return DistributedArray(recv_buff, dtype=recv_buff.dtype)
+
+    def multiply(self, dist_array):
+        if self.shape != dist_array.shape:
+            raise ValueError("Shape Mismatch")
+        recv_buff = np.empty(shape=self.shape, dtype=self.dtype)
+        self.base_comm.Allgatherv(self._local_array * dist_array._local_array,
+                                  recv_buff)
+        return DistributedArray(recv_buff, dtype=recv_buff.dtype)
+
+    def __repr__(self):
+        return f"<DistributedArray with shape={self.shape})" \
+               f", dtype={self.dtype}, " \
+               f"type={'Broadcast' if self.broad_cast else 'Scatter'}, " \
+               f"processes={[i for i in range(self.size)]})> "
---
+++ setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup, find_packages
+
+
+def src(pth):
+    return os.path.join(os.path.dirname(__file__), pth)
+
+
+# Project description
+descr = 'Python library implementing linear operators with MPI'
+
+# Setup
+setup(
+    name='pylops_mpi',
+    description=descr,
+    long_description=open(src('README.md')).read(),
+    long_description_content_type='text/markdown',
+    keywords=['algebra',
+              'inverse problems',
+              'large-scale optimization'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Scientific/Engineering :: Mathematics'
+    ],
+    install_requires=['numpy >= 1.15.0', 'scipy >= 1.4.0', 'mpi4py', 'pylops'],
+    packages=find_packages(exclude=['pytests']),
+    use_scm_version=dict(root='.',
+                         relative_to=__file__,
+                         write_to=src('pylops_mpi/version.py')),
+    setup_requires=['pytest-runner', 'setuptools_scm'],
+    test_suite='pytests',
+    tests_require=['pytest'],
+    zip_safe=True)
