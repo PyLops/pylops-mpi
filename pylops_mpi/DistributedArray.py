79aef5d1a2f8bea8eb81c5dbec05f1f2d706e7f3
rohanbabbar04 <rohanbabbar0408@yahoo.com>
2023-05-23 20:17:08 +0530

    Added New method for creating local arrays at each rank

--- pylops_mpi/DistributedArray.py
+++ pylops_mpi/DistributedArray.py
@@ -1,81 +1,89 @@
 import numpy as np
-from typing import Optional, Union
-
+from typing import Optional, Union, Tuple
+from numbers import Integral
 from mpi4py import MPI
 
-from pylops.utils import DTypeLike, NDArray, ShapeLike
+from pylops.utils import DTypeLike, NDArray
 
 
-class DistributedArray:
+class DistributedArray(np.ndarray):
     """Distributed Numpy Arrays
     Multidimensional NumPy-like distributed arrays.
     It brings NumPy arrays to high-performance computing
 
+    It extends the `numpy.ndarray` class
+
     Attributes
     ----------
-    shape : :obj:`tuple(int, int)`
-        Shape of the global array
+    global_shape : :obj:`tuple`
+        Shape of the global array.
     dtype : :obj:`str`, optional
         Type of elements in input array. Defaults to ``float``.
+    base_comm : :obj:`MPI.Comm`, optional
+        MPI Communicator over which array is distributed. Defaults to ``MPI.COMM_WORLD``.
+    type_part : :obj:`str`, optional
+        Broadcast or Scatter the array. Defaults to ``S``.
     """
 
-    def __init__(self, shape: Optional[ShapeLike] = None,
-                 dtype: Optional[DTypeLike] = 'float') -> None:
-        self.dtype = dtype
-        self.shape = shape
-        self.base_comm = MPI.COMM_WORLD
-        self.size = self.base_comm.Get_size()
-        self.rank = self.base_comm.Get_rank()
-        self.local_array = None
+    def __new__(cls, global_shape: Union[Tuple, Integral], dtype: Optional[DTypeLike] = "float",
+                base_comm: Optional[MPI.Comm] = MPI.COMM_WORLD, type_part: str = "S"):
+        if isinstance(global_shape, Integral):
+            global_shape = (global_shape,)
+        # Broadcast the array
+        if type_part == "B":
+            local_shape = global_shape
+        # Scatter the array
+        else:
+            local_shape = [((global_shape[0] // base_comm.Get_size()) + 1)
+                           if base_comm.Get_rank() < (
+                    global_shape[0] % base_comm.Get_size())
+                           else global_shape[0] // base_comm.Get_size()] + list(global_shape[1:])
+        # create an empty numpy local array
+        arr = np.ndarray.__new__(cls, local_shape, dtype)
+        arr._global_shape = global_shape
+        arr._local_shape = tuple(local_shape)
+        arr._base_comm = base_comm
+        return arr
 
     @property
-    def shape(self):
-        return self._shape
+    def global_shape(self):
+        """Global Shape of the array
 
-    @shape.setter
-    def shape(self, new_shape):
-        self._shape = new_shape
-
-    @shape.deleter
-    def shape(self):
-        del self._shape
-
-    def assign(self, x: NDArray):
-        pass
-
-    def broadcast(self, x: NDArray) -> None:
-        """Broadcast the NumPy Array
-
-        Parameters
-        ----------
-        x : :obj:`np.ndarray`
-            Array to be broadcasted
+        Returns
+        -------
+        global_shape : :obj:`tuple`
         """
-        if x is not None:
-            self.local_array = np.empty(x.shape, dtype=self.dtype)
-            if self.rank == 0:
-                self.local_array = x.astype(self.dtype)
-        self.base_comm.Bcast(self.local_array, root=0)
+        return self._global_shape
 
-    def scatter(self, x: NDArray):
-        """Scatter the NumPy Array
+    @property
+    def base_comm(self):
+        """Base MPI Communicator
 
-        Parameters
-        ----------
-        x : :obj:`np.ndarray`
-            Array to be scattered
+         Returns
+        -------
+        base_comm : :obj:`MPI.Comm`
         """
-        send_data = None
-        if self.rank == 0:
-            arrs = np.array_split(x, self.size)
-            raveled = [np.ravel(arr) for arr in arrs]
-            send_data = np.concatenate(raveled, dtype=self.dtype)
-        recv_shape = list(x.shape)
-        recv_shape_new = [((recv_shape[0] // self.size) + 1) if self.rank < (
-                recv_shape[0] % self.size)
-                          else recv_shape[0] // self.size] + recv_shape[1:]
-        self.local_array = np.empty(recv_shape_new, dtype=self.dtype)
-        self.base_comm.Scatterv(send_data, self.local_array, root=0)
+        return self._base_comm
+
+    @property
+    def local_shape(self):
+        """Local Shape of the Distributed array
+
+        Returns
+        -------
+        local_shape : :obj:`tuple`
+        """
+        return self._local_shape
+
+    @property
+    def local_array(self):
+        """View of the Local Array
+
+        Returns
+        -------
+        local_array : :obj:`np.ndarray`
+        """
+        return self.view(np.ndarray)
 
     def get_local_arrays(self):
         """Gather all the local arrays
@@ -84,7 +92,6 @@ class DistributedArray:
         final_array : :obj:`np.ndarray`
             Global Array gathered at all ranks
         """
-
         final_array = self.base_comm.allgather(self.local_array)
         return final_array
 
@@ -99,9 +106,8 @@ class DistributedArray:
         """
         if self.shape != dist_array.shape:
             raise ValueError("Shape Mismatch")
-        recv_buff = np.empty(shape=self.shape, dtype=self.dtype)
-        SumArray = DistributedArray(recv_buff.shape, recv_buff.dtype)
-        SumArray.local_array = self.local_array + dist_array.local_array
+        SumArray = DistributedArray(self.global_shape, dtype=self.dtype)
+        SumArray[:] = self.local_array + dist_array.local_array
         return SumArray
 
     def multiply(self, dist_array):
@@ -109,9 +115,8 @@ class DistributedArray:
         """
         if self.shape != dist_array.shape:
             raise ValueError("Shape Mismatch")
-        recv_buff = np.empty(shape=self.shape, dtype=self.dtype)
-        ProductArray = DistributedArray(recv_buff.shape, recv_buff.dtype)
-        ProductArray.local_array = self.local_array * dist_array.local_array
+        ProductArray = DistributedArray(self.global_shape, self.dtype)
+        ProductArray[:] = self.local_array * dist_array.local_array
         return ProductArray
 
     def dot(self, dist_array):
@@ -125,6 +130,41 @@ class DistributedArray:
         pass
 
     def __repr__(self):
-        return f"<DistributedArray with shape={self.shape})" \
+        return f"<DistributedArray with shape={self.global_shape})" \
                f", dtype={self.dtype}, " \
-               f"processes={[i for i in range(self.size)]})> "
+               f"processes={[i for i in range(self.base_comm.Get_size())]})> "
+
+    # def broadcast(self, x: NDArray) -> None:
+    #     """Broadcast the NumPy Array
+    #
+    #     Parameters
+    #     ----------
+    #     x : :obj:`np.ndarray`
+    #         Array to be broadcasted
+    #     """
+    #     local_array = None
+    #     if x is not None:
+    #         local_array = np.empty(x.shape, dtype=self.dtype)
+    #         if self.base_comm.Get_rank() == 0:
+    #             local_array = x.astype(self.dtype)
+    #     self.base_comm.bcast(local_array, root=0)
+    #
+    # def scatter(self, x: NDArray):
+    #     """Scatter the NumPy Array
+    #
+    #     Parameters
+    #     ----------
+    #     x : :obj:`np.ndarray`
+    #         Array to be scattered
+    #     """
+    #     send_data = None
+    #     if self.base_comm.Get_rank() == 0:
+    #         arrs = np.array_split(x, self.size)
+    #         raveled = [np.ravel(arr) for arr in arrs]
+    #         send_data = np.concatenate(raveled, dtype=self.dtype)
+    #     recv_shape = list(x.shape)
+    #     recv_shape_new = [((recv_shape[0] // self.size) + 1) if self.base_comm.Get_rank() < (
+    #             recv_shape[0] % self.size)
+    #                       else recv_shape[0] // self.size] + recv_shape[1:]
+    #     self.local_array = np.empty(recv_shape_new, dtype=self.dtype)
+    #     self.base_comm.Scatterv(send_data, self.local_array, root=0)
