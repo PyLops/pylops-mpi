03e5256a8fbde26b88a09cc7beccaab04fa07a85
rohanbabbar04 <rohanbabbar0408@yahoo.com>
2023-06-29 22:23:36 +0530

    Added tests

--- tests/test_blockdiag.py
+++ tests/test_blockdiag.py
@@ -1,7 +1,8 @@
+from mpi4py import MPI
 import numpy as np
 from numpy.testing import assert_allclose
-
 np.random.seed(42)
+
 import pytest
 
 import pylops
@@ -16,16 +17,33 @@ par2j = {'ny': 301, 'nx': 101, 'dtype': np.complex128}
 @pytest.mark.mpi(min_size=2)
 @pytest.mark.parametrize("par", [(par1), (par1j), (par2), (par2j)])
 def test_blockdiag(par):
-    G1 = np.random.normal(0, 10, (par['ny'], par['nx'])).astype(par['dtype'])
-    G2 = np.random.normal(0, 10, (par['ny'], par['nx'])).astype(par['dtype'])
-    ops = [pylops.Identity(par['ny'], par['nx']),
-           pylops.Zero(par['ny'], par['nx']), pylops.MatrixMult(G1),
-           pylops.MatrixMult(G2)]
-    BDiag_MPI = pylops_mpi.MPIBlockDiag(ops=ops)
-    assert isinstance(BDiag_MPI, pylops.LinearOperator)
-    BDiag = pylops.BlockDiag(ops=ops)
-    x = np.random.normal(100, 100, (BDiag.shape[1],))
-    y = np.random.normal(100, 100, (BDiag.shape[0],))
-    assert BDiag_MPI.shape == BDiag.shape
-    assert_allclose(BDiag_MPI * x, BDiag * x, rtol=1e-14)
-    assert_allclose(BDiag_MPI.H * y, BDiag.H * y, rtol=1e-14)
+    size = MPI.COMM_WORLD.Get_size()
+    rank = MPI.COMM_WORLD.Get_rank()
+    Op = pylops.MatrixMult(A=((rank + 1) * np.ones(shape=(par['ny'], par['nx']))).astype(par['dtype']))
+    BDiag_MPI = pylops_mpi.MPIBlockDiag(ops=[Op, ])
+
+    x = pylops_mpi.DistributedArray(global_shape=size * par['nx'], dtype=par['dtype'])
+    x[:] = np.ones(shape=par['nx'], dtype=par['dtype'])
+    x_global = x.asarray()
+
+    y = pylops_mpi.DistributedArray(global_shape=size * par['ny'], dtype=par['dtype'])
+    y[:] = np.ones(shape=par['ny'], dtype=par['dtype'])
+    y_global = y.asarray()
+
+    x_mat = BDiag_MPI._matvec(x)
+    y_rmat = BDiag_MPI._rmatvec(y)
+    assert isinstance(x_mat, pylops_mpi.DistributedArray)
+    assert isinstance(y_rmat, pylops_mpi.DistributedArray)
+
+    x_mat_mpi = x_mat.asarray()
+    y_rmat_mpi = y_rmat.asarray()
+
+    if rank == 0:
+        ops = [pylops.MatrixMult((i + 1) * np.ones(shape=(par['ny'], par['nx'])).astype(par['dtype'])) for i in range(size)]
+        BDiag = pylops.BlockDiag(ops=ops)
+
+        print(x_global.shape)
+        x_mat_np = BDiag @ x_global
+        y_rmat_np = BDiag.H @ y_global
+        assert_allclose(x_mat_mpi, x_mat_np, rtol=1e-14)
+        assert_allclose(y_rmat_mpi, y_rmat_np, rtol=1e-14)
