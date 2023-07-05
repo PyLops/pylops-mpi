977e5e8f3ac12575a1216f89fb93b1ad8ba21ef4
rohanbabbar04 <rohanbabbar0408@yahoo.com>
2023-07-05 15:26:35 +0530

    Added testdata and updated code

--- examples/plot_post_stack_inversion.py
+++ examples/plot_post_stack_inversion.py
@@ -25,25 +25,20 @@ size = MPI.COMM_WORLD.Get_size()
 nt0 = 301
 dt0 = 0.004
 t0 = np.arange(nt0) * dt0
-nx = 150
-# Size of y at each rank
-ny_i = 25
+model = np.load("../testdata/poststack_model.npz")
+x = model['x'][::3] / 1000.0
+z = model['z'] / 1000.0
+nx, nz, ny_i = len(x), len(z), 10
+m = np.log(model['model'][:, ::3])  # shape=(nz, nx)
 # Size of y at all ranks
 ny = MPI.COMM_WORLD.allreduce(ny_i)
-nz = 95
-x = 800 + 4 * np.arange(nx)
-x = x / 1000.0
-z = 800 + 4 * np.arange(nz)
-z = z / 1000.0
-m = np.log((rank + 1) * np.random.uniform(1.8, 3.8, (nx, nz)))
-y_i = (rank + 1) * np.arange(ny_i)
 # Extending over first axis
 m3d = np.tile(m[np.newaxis, :, :], (ny_i, 1, 1))
 
 # Smooth model
-nsmoothx, nsmoothz = 30, 20
-mback = filtfilt(np.ones(nsmoothx) / float(nsmoothx), 1, m, axis=0)
-mback = filtfilt(np.ones(nsmoothz) / float(nsmoothz), 1, mback, axis=1)
+nsmoothz, nsmoothx = 30, 20
+mback = filtfilt(np.ones(nsmoothz) / float(nsmoothz), 1, m, axis=0)
+mback = filtfilt(np.ones(nsmoothx) / float(nsmoothx), 1, mback, axis=1)
 # Extending over first axis
 mback3d = np.tile(mback[np.newaxis, :, :], (ny_i, 1, 1))
 
@@ -74,25 +69,27 @@ PPop_dense = PoststackLinearModelling(wav, nt0=nz, spatdims=(ny_i, nx), explicit
 BDiag_dense = MPIBlockDiag(ops=[PPop_dense, ])
 # Forward
 d_dense_dist = BDiag_dense * m3d_dist
+d_dense = d_dense_dist.asarray().reshape((ny, nz, nx))
 # Adjoint
 d_dense_adj_dist = BDiag_dense.H * m3d_dist
-d_dense = d_dense_dist.asarray().reshape((ny, nx, nz))
-d_dense_adj = d_dense_adj_dist.asarray().reshape((ny, nx, nz))
+d_dense_adj = d_dense_adj_dist.asarray().reshape((ny, nz, nx))
 
 if rank == 0:
     fig, axs = plt.subplots(nrows=1, ncols=4, figsize=(15, 9))
-    axs[0].imshow(d_dense[7, :, :], cmap="gray", vmin=d_dense.min(), vmax=d_dense.max(), extent=(x[0], x[-1], z[-1], z[0]))
+    axs[0].imshow(d_dense[3, :, :], cmap="gray", vmin=d_dense.min(), vmax=d_dense.max(),
+                  extent=(x[0], x[-1], z[-1], z[0]))
     axs[0].set_title("Data")
     axs[0].axis("tight")
 
-    axs[1].imshow(d_dense_adj[7, :, :], cmap="gray", vmin=d_dense_adj.min(), vmax=d_dense_adj.max(), extent=(x[0], x[-1], z[-1], z[0]))
+    axs[1].imshow(d_dense_adj[3, :, :], cmap="gray", vmin=d_dense_adj.min(), vmax=d_dense_adj.max(),
+                  extent=(x[0], x[-1], z[-1], z[0]))
     axs[1].set_title("Adjoint")
     axs[1].axis("tight")
 
-    axs[2].imshow(m3d[7, :, :], cmap="gist_rainbow", vmin=m.min(), vmax=m.max(), extent=(x[0], x[-1], z[-1], z[0]))
+    axs[2].imshow(m3d[3, :, :], cmap="gist_rainbow", vmin=m.min(), vmax=m.max(), extent=(x[0], x[-1], z[-1], z[0]))
     axs[2].set_title("Model")
     axs[2].axis("tight")
 
-    axs[3].imshow(mback3d[7, :, :], cmap="gist_rainbow", vmin=m.min(), vmax=m.max(), extent=(x[0], x[-1], z[-1], z[0]))
+    axs[3].imshow(mback3d[3, :, :], cmap="gist_rainbow", vmin=m.min(), vmax=m.max(), extent=(x[0], x[-1], z[-1], z[0]))
     axs[3].set_title("Smooth Model")
     axs[3].axis("tight")
