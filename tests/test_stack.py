5e8ca7f8216314d8376bbd33c4bd76bc472ae2cd
Rohan Babbar <rohanbabbar0408@yahoo.com>
2023-07-19 09:14:55 +0530

    Added MPIHStack (#45)
    * Added MPIHStack
    
    * Added tests
    
    * Minor:Added docs and updated __init__
    
    * Minor: Import update
    
    * Minor: Handle self.ops

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -51,3 +51,44 @@ def test_vstack(par):
         y_rmat_np = VStack.H @ y_global
         assert_allclose(x_mat_mpi, x_mat_np, rtol=1e-14)
         assert_allclose(y_rmat_mpi, y_rmat_np, rtol=1e-14)
+
+
+@pytest.mark.mpi(min_size=2)
+@pytest.mark.parametrize("par", [(par1), (par1j), (par2), (par2j)])
+def test_hstack(par):
+    """Test the MPIHStack operator"""
+    size = MPI.COMM_WORLD.Get_size()
+    rank = MPI.COMM_WORLD.Get_rank()
+    A = np.ones(shape=(par['ny'], par['nx'])) + par['imag'] * np.ones(shape=(par['ny'], par['nx']))
+    Op = pylops.MatrixMult(A=((rank + 1) * A).astype(par['dtype']))
+    HStack_MPI = pylops_mpi.MPIHStack(ops=[Op, ])
+
+    # Scattered DistributedArray
+    x = pylops_mpi.DistributedArray(global_shape=size * par['nx'],
+                                    partition=pylops_mpi.Partition.SCATTER,
+                                    dtype=par['dtype'])
+    x[:] = np.ones(shape=par['nx'], dtype=par['dtype'])
+    x_global = x.asarray()
+
+    # Broadcasted DistributedArray(global_shape == local_shape)
+    y = pylops_mpi.DistributedArray(global_shape=par['ny'],
+                                    partition=pylops_mpi.Partition.BROADCAST,
+                                    dtype=par['dtype'])
+    y[:] = np.ones(shape=par['ny'], dtype=par['dtype'])
+    y_global = y.asarray()
+
+    x_mat = HStack_MPI @ x
+    y_rmat = HStack_MPI.H @ y
+    assert isinstance(x_mat, pylops_mpi.DistributedArray)
+    assert isinstance(y_rmat, pylops_mpi.DistributedArray)
+
+    x_mat_mpi = x_mat.asarray()
+    y_rmat_mpi = y_rmat.asarray()
+
+    if rank == 0:
+        ops = [pylops.MatrixMult(A=((i + 1) * A).astype(par['dtype'])) for i in range(size)]
+        HStack = pylops.HStack(ops=ops)
+        x_mat_np = HStack @ x_global
+        y_rmat_np = HStack.H @ y_global
+        assert_allclose(x_mat_mpi, x_mat_np, rtol=1e-14)
+        assert_allclose(y_rmat_mpi, y_rmat_np, rtol=1e-14)
