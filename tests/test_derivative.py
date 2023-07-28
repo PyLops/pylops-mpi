1d2938684026625b556616e2747ffc0d081fad75
rohanbabbar04 <rohanbabbar0408@yahoo.com>
2023-07-28 20:51:29 +0530

    Added tests for Broadcast

--- tests/test_derivative.py
+++ tests/test_derivative.py
@@ -14,96 +14,141 @@ par1 = {
     "nz": 100,
     "dz": 1.0,
     "edge": False,
-    "dtype": np.float128
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.SCATTER
+}
+
+par1b = {
+    "nz": 100,
+    "dz": 1.0,
+    "edge": False,
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.BROADCAST
 }
 
 par1j = {
     "nz": 100,
     "dz": 1.0,
     "edge": False,
-    "dtype": np.complex256
+    "dtype": np.complex256,
+    "partition": pylops_mpi.Partition.SCATTER
 }
 
 par1e = {
     "nz": 100,
     "dz": 1.0,
     "edge": True,
-    "dtype": np.float128
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.SCATTER
 }
 
 par2 = {
     "nz": 101,
     "dz": 1.0,
     "edge": False,
-    "dtype": np.float128
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.SCATTER
+}
+
+par2b = {
+    "nz": 101,
+    "dz": 1.0,
+    "edge": False,
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.BROADCAST
 }
 
 par2j = {
     "nz": 101,
     "dz": 1.0,
     "edge": False,
-    "dtype": np.complex256
+    "dtype": np.complex256,
+    "partition": pylops_mpi.Partition.SCATTER
 }
 
 par2e = {
     "nz": 101,
     "dz": 1.0,
     "edge": True,
-    "dtype": np.float128
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.SCATTER
 }
 
 par3 = {
     "nz": 100,
     "dz": 0.4,
     "edge": False,
-    "dtype": np.float128
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.SCATTER
+}
+
+par3b = {
+    "nz": 100,
+    "dz": 0.4,
+    "edge": False,
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.BROADCAST
 }
 
 par3j = {
     "nz": 100,
     "dz": 0.4,
     "edge": True,
-    "dtype": np.complex256
+    "dtype": np.complex256,
+    "partition": pylops_mpi.Partition.SCATTER
 }
 
 par3e = {
     "nz": 100,
     "dz": 0.4,
     "edge": True,
-    "dtype": np.float128
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.SCATTER
 }
 
 par4 = {
     "nz": 101,
     "dz": 0.4,
     "edge": False,
-    "dtype": np.float128
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.SCATTER
+}
+
+par4b = {
+    "nz": 101,
+    "dz": 0.4,
+    "edge": False,
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.BROADCAST
 }
 
 par4j = {
     "nz": 101,
     "dz": 0.4,
     "edge": True,
-    "dtype": np.complex256
+    "dtype": np.complex256,
+    "partition": pylops_mpi.Partition.SCATTER
 }
 
 par4e = {
     "nz": 101,
     "dz": 0.4,
     "edge": True,
-    "dtype": np.float128
+    "dtype": np.float128,
+    "partition": pylops_mpi.Partition.SCATTER
 }
 
 
 @pytest.mark.mpi(min_size=2)
-@pytest.mark.parametrize("par", [(par1), (par1j), (par1e), (par2), (par2j),
-                                 (par2e), (par3), (par3j), (par3e), (par4),
-                                 (par4j), (par4e)])
+@pytest.mark.parametrize("par", [(par1), (par1b), (par1j), (par1e), (par2), (par2b),
+                                 (par2j), (par2e), (par3), (par3b), (par3j), (par3e),
+                                 (par4), (par4b), (par4j), (par4e)])
 def test_first_derivative_forward(par):
     Fop_MPI = pylops_mpi.MPIFirstDerivative(dims=par['nz'], sampling=par['dz'],
                                             kind="forward", edge=par['edge'],
                                             dtype=par['dtype'])
-    x = pylops_mpi.DistributedArray(global_shape=par['nz'], dtype=par['dtype'])
+    x = pylops_mpi.DistributedArray(global_shape=par['nz'], dtype=par['dtype'],
+                                    partition=par['partition'])
     x[:] = np.random.normal(rank, 10, x.local_shape)
     x_global = x.asarray()
     # Forward
@@ -125,14 +170,15 @@ def test_first_derivative_forward(par):
 
 
 @pytest.mark.mpi(min_size=2)
-@pytest.mark.parametrize("par", [(par1), (par1j), (par1e), (par2), (par2j),
-                                 (par2e), (par3), (par3j), (par3e), (par4),
-                                 (par4j), (par4e)])
+@pytest.mark.parametrize("par", [(par1), (par1b), (par1j), (par1e), (par2), (par2b),
+                                 (par2j), (par2e), (par3), (par3b), (par3j), (par3e),
+                                 (par4), (par4b), (par4j), (par4e)])
 def test_first_derivative_backward(par):
     Fop_MPI = pylops_mpi.MPIFirstDerivative(dims=par['nz'], sampling=par['dz'],
                                             kind="backward", edge=par['edge'],
                                             dtype=par['dtype'])
-    x = pylops_mpi.DistributedArray(global_shape=par['nz'], dtype=par['dtype'])
+    x = pylops_mpi.DistributedArray(global_shape=par['nz'], dtype=par['dtype'],
+                                    partition=par['partition'])
     x[:] = np.random.normal(rank, 10, x.local_shape)
     x_global = x.asarray()
     # Forward
@@ -154,15 +200,16 @@ def test_first_derivative_backward(par):
 
 
 @pytest.mark.mpi(min_size=2)
-@pytest.mark.parametrize("par", [(par1), (par1j), (par1e), (par2), (par2j),
-                                 (par2e), (par3), (par3j), (par3e), (par4),
-                                 (par4j), (par4e)])
+@pytest.mark.parametrize("par", [(par1), (par1b), (par1j), (par1e), (par2), (par2b),
+                                 (par2j), (par2e), (par3), (par3b), (par3j), (par3e),
+                                 (par4), (par4b), (par4j), (par4e)])
 def test_first_derivative_centered(par):
     for order in [3, 5]:
         Fop_MPI = pylops_mpi.MPIFirstDerivative(dims=par['nz'], sampling=par['dz'],
                                                 kind="centered", edge=par['edge'],
                                                 order=order, dtype=par['dtype'])
-        x = pylops_mpi.DistributedArray(global_shape=par['nz'], dtype=par['dtype'])
+        x = pylops_mpi.DistributedArray(global_shape=par['nz'], dtype=par['dtype'],
+                                        partition=par['partition'])
         x[:] = np.random.normal(rank, 10, x.local_shape)
         x_global = x.asarray()
         # Forward
