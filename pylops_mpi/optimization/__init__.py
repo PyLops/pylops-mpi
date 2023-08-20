d4d2e34b0d0aa37f5bdac6fca63a0e2530d71a3f
rohanbabbar04 <rohanbabbar0408@yahoo.com>
2023-08-20 13:19:55 +0530

    Added docs

--- pylops_mpi/optimization/__init__.py
+++ pylops_mpi/optimization/__init__.py
@@ -6,6 +6,7 @@ The subpackage `optimization` provides solvers to be used with pylops_mpi linear
 
 A list of least-squares solvers in pylops_mpi.optimization.basic:
 
+    cg                              Conjugate gradient
     cgls                            Conjugate gradient least-squares.
 
 """
