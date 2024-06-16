59c7a077791c819bc1b3df577faa1ae6c205bf72
mrava87 <matteoravasi@gmail.com>
2024-06-16 15:55:27 +0300

    feat: added MPIFredholm1 and MPIMDC

---
+++ pylops_mpi/signalprocessing/__init__.py
@@ -0,0 +1,19 @@
+"""
+Signal Processing Operators using MPI
+=====================================
+
+The subpackage signalprocessing extends some of the signal processing
+functionalities in pylops.signalprocessing providing forward and adjoint 
+functionalities using MPI.
+
+A list of operators present in pylops_mpi.signalprocessing :
+    MPIFredholm1                      Fredholm integral of first kind.
+
+
+"""
+
+from .Fredholm1 import *
+
+__all__ = [
+    "MPIFredholm1",
+]
