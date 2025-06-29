069e5dd1ac5a150dd7c27338aa28edb335402be7
astroC86 <66444189+astroC86@users.noreply.github.com>
2025-06-29 03:37:50 +0200

    working simple example

--- examples/matrixmul.py
+++ examples/matrixmul.py
@@ -7,9 +7,9 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
 
-M = 16 #512
-N = 16 #512
-K = 16 #512
+M = 8 #512
+N = 8 #512
+K = 8 #512
 
 A_shape  = (M,K)
 B_shape  = (K,N)
@@ -17,8 +17,14 @@ C_shape  = (M,N)
 
 p_prime = math.isqrt(size)
 assert p_prime*p_prime == size
-A = np.arange(int(A_shape[0]*A_shape[1])).reshape(A_shape).reshape((M//p_prime,-1))
-B = np.arange(int(B_shape[0]*B_shape[1])).reshape(B_shape).reshape((K//p_prime,-1))
+
+# Create A with 2D block-cyclic structure
+A_data = np.arange(int(A_shape[0]*A_shape[1])).reshape(A_shape)
+A = A_data.reshape(p_prime, M//p_prime, p_prime, K//p_prime).transpose(1, 0, 2, 3).reshape(M//p_prime, -1)
+
+# Create B with 2D block-cyclic structure
+B_data = np.arange(int(B_shape[0]*B_shape[1])).reshape(B_shape)
+B = B_data.reshape(p_prime, K//p_prime, p_prime, N//p_prime).transpose(1, 0, 2, 3).reshape(K//p_prime, -1)
 
 A_dist = pylops_mpi.DistributedArray.to_dist(A,
                                           partition=pylops_mpi.Partition.SCATTER,
@@ -30,23 +36,26 @@ B_dist = pylops_mpi.DistributedArray.to_dist(B,
 C_dist = pylops_mpi.DistributedArray(global_shape=(M // p_prime, N * p_prime),
                                     partition=pylops_mpi.Partition.SCATTER,
                                      axis=1)
+if rank == 0: print(A_dist.local_array)
 
-
-
-p    = int(np.sqrt(size))
-i, j = divmod(rank, p)
+i, j = divmod(rank, p_prime)
 row_comm = comm.Split(color=i, key=j)
 col_comm = comm.Split(color=j, key=i)
 
-c = np.zeros((M//p, N//p), dtype=np.float32)
-for k in range(p):
-    Atemp = A_dist.local_array.copy() if j==k else np.empty_like(A_dist.local_array)
-    Btemp = B_dist.local_array.copy() if i==k else np.empty_like(B_dist.local_array)
-    if rank==0: print(k,"cast")
-    row_comm.Bcast([Atemp, MPI.FLOAT], root=k)
-    col_comm.Bcast([Btemp, MPI.FLOAT], root=k)
-    c += Atemp @ Btemp
-    if rank == 0: print(k,"after")
+c_local = np.zeros((M//p_prime, N//p_prime))
+for k in range(p_prime):
+    Atemp=A_dist.local_array.copy() if j==k else np.empty_like(A_dist.local_array)
+    Btemp=B_dist.local_array.copy() if i==k else np.empty_like(B_dist.local_array)
+    rootA=i*p_prime+k; rootB=k*p_prime+j
+    row_comm.Bcast([Atemp,MPI.FLOAT],root=k)
+    col_comm.Bcast([Btemp,MPI.FLOAT],root=k)
+    # print(f"[Rank {rank}] iter{k} after : received A from {rootA}, B from {rootB}, A0={Atemp.flat[0]},B0={Btemp.flat[0]}")
+    c_local += Atemp @ Btemp
 
-C_dist.local_array[:] = c
-if rank==0: print(C_dist.asarray())
+C_dist.local_array[:] = c_local
+C = C_dist.asarray().reshape((M,N))
+A_ = A_dist.asarray().reshape((M,K))
+B_ = B_dist.asarray().reshape((K,N))
+if rank == 0 :
+    print(A_data @ B_data)
+    print(C)
