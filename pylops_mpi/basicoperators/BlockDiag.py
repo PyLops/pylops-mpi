f13c237241e433ca28382552e0d75383d6613424
rohanbabbar04 <rohanbabbar0408@gmail.com>
2026-03-08 00:46:15 +0530

    Support numpy2.4

--- pylops_mpi/basicoperators/BlockDiag.py
+++ pylops_mpi/basicoperators/BlockDiag.py
@@ -170,8 +170,8 @@ class MPIStackedBlockDiag(MPIStackedLinearOperator):
                  dtype: Optional[DTypeLike] = None):
         self.ops = ops
         dtype = _get_dtype(self.ops) if dtype is None else np.dtype(dtype)
-        shape = (int(np.sum(op.shape[0] for op in ops)),
-                 int(np.sum(op.shape[1] for op in ops)))
+        shape = (int(sum(op.shape[0] for op in ops)),
+                 int(sum(op.shape[1] for op in ops)))
         super().__init__(shape=shape, dtype=dtype, base_comm=base_comm)
 
     def _matvec(self, x: StackedDistributedArray) -> StackedDistributedArray:
